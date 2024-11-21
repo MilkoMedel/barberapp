7eee8b3cbf5d9096553ac517428c283c06c50bcc
Patricio Fischi <patofinschi@gmail.com>
2024-11-21 00:12:24 -0300

    Implementacion firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,SharedModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"barberapp-2dd9c","appId":"1:572015083375:web:e447b288947f048e91329a","storageBucket":"barberapp-2dd9c.firebasestorage.app","apiKey":"AIzaSyBu2sizQ29cWo5N7T1E_BDrvDGExdqWxGY","authDomain":"barberapp-2dd9c.firebaseapp.com","messagingSenderId":"572015083375","measurementId":"G-27X6MJ78VM"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBu2sizQ29cWo5N7T1E_BDrvDGExdqWxGY",
+    authDomain: "barberapp-2dd9c.firebaseapp.com",
+    projectId: "barberapp-2dd9c",
+    storageBucket: "barberapp-2dd9c.firebasestorage.app",
+    messagingSenderId: "572015083375",
+    appId: "1:572015083375:web:e447b288947f048e91329a",
+    measurementId: "G-27X6MJ78VM"
+  }
 };
 
 /*
