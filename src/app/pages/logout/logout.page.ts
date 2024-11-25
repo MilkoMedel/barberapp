4c0d9ab71c02083d718da790cb77fb99b42ba290
Patricio Fischi <patofinschi@gmail.com>
2024-11-24 21:49:54 -0300

    Cambios logout

--- src/app/pages/logout/logout.page.ts
+++ src/app/pages/logout/logout.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router'; // Importa Router
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/firebase/auth.service';
 
@@ -8,10 +9,14 @@ import { AuthService } from 'src/app/firebase/auth.service';
 })
 export class LogoutPage implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) {} // Inyecta Router
 
   ngOnInit() {
-    this.authService.logout();
+    this.authService.logout().then(() => {
+      // Redirigir a la página de login tras cerrar sesión
+      this.router.navigateByUrl('/login');
+    }).catch(error => {
+      console.error('Error al cerrar sesión:', error);
+    });
   }
-
 }
